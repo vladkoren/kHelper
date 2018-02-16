bf4df38433f63cf3cc20fe787c03c11c367ce738
vladkoren <vladkorenevsky@gmail.com>
2018-02-17 00:00:55 +0200

    Initial commit

---
+++ app/src/main/java/com/koren/khelper/MainActivity.java
@@ -0,0 +1,31 @@
+package com.koren.khelper;
+
+import android.content.Intent;
+import android.icu.text.IDNA;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mathBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mathBtn = findViewById(R.id.mathBtn);
+    }
+
+    public void mathBtnPresssed(View view) {
+        Intent intent = new Intent(MainActivity.this, MathSelectActivity.class);
+        startActivity(intent);
+    }
+
+    public void infoBtnPressed(View view) {
+        Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+        startActivity(intent);
+        
+    }
+}
---
+++ app/src/main/java/com/koren/khelper/QuadraticEquationActivity.java
@@ -0,0 +1,40 @@
+package com.koren.khelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class QuadraticEquationActivity extends AppCompatActivity {
+
+    double kA, kB, kC, D, x1, x2;
+    EditText aEditText, bEditText, cEditText;
+    TextView resultsField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quadratic_equation);
+        aEditText = findViewById(R.id.aEditText);
+        bEditText = findViewById(R.id.bEditText);
+        cEditText = findViewById(R.id.cEditText);
+        resultsField = findViewById(R.id.resultsText);
+    }
+
+    public void solveIt(View view) {
+        kA = Integer.parseInt(aEditText.getText().toString());
+        kB = Integer.parseInt(bEditText.getText().toString());
+        kC = Integer.parseInt(cEditText.getText().toString());
+        D = (Math.pow(kB, 2) - (4*kA*kC));
+        if(D>=0){
+            x1 = ((-1*kB)+Math.sqrt(D))/(2*kA);
+            x2 = ((-1*kB)-Math.sqrt(D))/(2*kA);
+            resultsField.setText("D = " + D + " x1 = " + x1 + " x2 = " + x2);
+        }
+        else {
+            resultsField.setText(getResources().getString(R.string.noSolutions));
+        }
+    }
+}
